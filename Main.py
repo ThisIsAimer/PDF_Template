1d3823626d7dbb42f82e4509083d5c90ef579b3f
raunak <raunakbiswas05@gmail.com>
2024-12-05 11:34:33 +0530

    Writing in pdfs with python!

--- Main.py
+++ Main.py
@@ -8,6 +8,14 @@ pdf.set_font(family="times",style="B",size=12)
 pdf.cell(w=0,h=12,txt="hello there!",align="L",ln=1,border=1)
 
 pdf.set_font(family="times",style="B",size=10)
-pdf.cell(w=0,h=12,txt="hi there",align="L",ln=1)
+pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
+
+pdf.add_page()
+pdf.set_font(family="times",style="B",size=12)
+
+pdf.cell(w=0,h=12,txt="next page!",align="L",ln=0,border=1)
+
+pdf.set_font(family="times",style="B",size=10)
+pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
 
 pdf.output("output.pdf")
