dd17d9d15d98af885314e0a9e26625ab7b7a8736
raunak <raunakbiswas05@gmail.com>
2024-12-05 11:28:54 +0530

    Writing in pdfs with python!

--- Main.py
+++ Main.py
@@ -3,4 +3,11 @@ from fpdf import FPDF
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
 pdf.add_page()
+pdf.set_font(family="times",style="B",size=12)
+#txt is written as text in actual fpdf documentation but doesn't work for some reason
+pdf.cell(w=0,h=12,txt="hello there!",align="L",ln=1,border=1)
+
+pdf.set_font(family="times",style="B",size=10)
+pdf.cell(w=0,h=12,txt="hi there",align="L",ln=1)
+
 pdf.output("output.pdf")
