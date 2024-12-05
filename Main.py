eed4c9d4e00f25488ed3d76031be238313499111
raunak <raunakbiswas05@gmail.com>
2024-12-05 12:48:35 +0530

    pdf_maker with header, a footer and some other changes

--- Main.py
+++ Main.py
@@ -5,31 +5,27 @@ import pandas
 data  = pandas.read_csv("topics.csv",sep=",")
 print(data)
 pdf = FPDF(orientation="P",unit="mm",format="A4")
+pdf.set_auto_page_break(False,0)
 
 for columns, rows in data.iterrows():
     pdf.add_page()
-    pdf.set_font(family="times", style="B", size=20)
-    pdf.cell(w=0, h=12, txt=f"{rows['Topic']}", align="L", ln=1,border=1)
+    pdf.set_font(family="times", style="B", size=24)
+    pdf.set_text_color(100,100,100) #RGB
+    pdf.cell(w=0, h=12, txt=f"{rows['Topic']}", align="L", ln=1)
+    pdf.line(10,22,200,22)
+
+    pdf.ln(260)
+    pdf.set_font(family="times", style="B", size=8)
+    pdf.set_text_color(100, 100, 100)  # RGB
+    pdf.cell(w=0, h=8, txt=f"{rows['Topic']}", align="R", ln=1)
+
     pages = int(rows["Pages"])
     for i in range(pages-1):
         pdf.add_page()
+        pdf.ln(272)
+        pdf.set_font(family="times", style="B", size=8)
+        pdf.set_text_color(100, 100, 100)  # RGB
+        pdf.cell(w=0, h=8, txt=f"{rows['Topic']}", align="R", ln=1)
 
 
-#
-# pdf.add_page()
-# pdf.set_font(family="times",style="B",size=12)
-# #txt is written as text in actual fpdf documentation but doesn't work for some reason
-# pdf.cell(w=0,h=12,txt="hello there!",align="L",ln=1,border=1)
-#
-# pdf.set_font(family="times",style="B",size=10)
-# pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
-#
-# pdf.add_page()
-# pdf.set_font(family="times",style="B",size=12)
-#
-# pdf.cell(w=0,h=12,txt="next page!",align="L",ln=0,border=1)
-#
-# pdf.set_font(family="times",style="B",size=10)
-# pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
-#
 pdf.output("output.pdf")
