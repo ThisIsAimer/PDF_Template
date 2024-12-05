f29f3c92faa5787eb797eb4fe9aa95b80f0b356f
raunak <raunakbiswas05@gmail.com>
2024-12-05 12:26:25 +0530

    Making pdf pages with pandas csv reading and FPDF

--- Main.py
+++ Main.py
@@ -1,21 +1,35 @@
 from fpdf import FPDF
+import pandas
 
 
+data  = pandas.read_csv("topics.csv",sep=",")
+print(data)
 pdf = FPDF(orientation="P",unit="mm",format="A4")
-pdf.add_page()
-pdf.set_font(family="times",style="B",size=12)
-#txt is written as text in actual fpdf documentation but doesn't work for some reason
-pdf.cell(w=0,h=12,txt="hello there!",align="L",ln=1,border=1)
 
-pdf.set_font(family="times",style="B",size=10)
-pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
+for columns, rows in data.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="times", style="B", size=20)
+    pdf.cell(w=0, h=12, txt=f"{rows['Topic']}", align="L", ln=1,border=1)
+    pages = int(rows["Pages"])
+    for i in range(pages-1):
+        pdf.add_page()
 
-pdf.add_page()
-pdf.set_font(family="times",style="B",size=12)
 
-pdf.cell(w=0,h=12,txt="next page!",align="L",ln=0,border=1)
-
-pdf.set_font(family="times",style="B",size=10)
-pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
-
+#
+# pdf.add_page()
+# pdf.set_font(family="times",style="B",size=12)
+# #txt is written as text in actual fpdf documentation but doesn't work for some reason
+# pdf.cell(w=0,h=12,txt="hello there!",align="L",ln=1,border=1)
+#
+# pdf.set_font(family="times",style="B",size=10)
+# pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
+#
+# pdf.add_page()
+# pdf.set_font(family="times",style="B",size=12)
+#
+# pdf.cell(w=0,h=12,txt="next page!",align="L",ln=0,border=1)
+#
+# pdf.set_font(family="times",style="B",size=10)
+# pdf.cell(w=0,h=12,txt="hi there",align="R",ln=1)
+#
 pdf.output("output.pdf")
